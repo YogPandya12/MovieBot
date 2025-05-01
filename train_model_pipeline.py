96ab9c717d0dddf103c92613da75c37c8370c162
YogPandya12 <pandyayog33@gmail.com>
2025-05-01 21:04:32 +0530

    Update train_model_pipeline.py

--- train_model_pipeline.py
+++ train_model_pipeline.py
@@ -19,17 +19,13 @@ with open('artifacts\intents.json', 'r') as f:
 all_words = []
 tags = []
 xy = []
-# Process each intent in the intents file
+
 for intent in intents['intents']:
     tag = intent['tag']
-    # add to tag list
     tags.append(tag)
     for pattern in intent['patterns']:
-        # tokenize each word in the sentence
         w = tokenize(pattern)
-        # add to our words list
         all_words.extend(w)
-        # add to xy pair
         xy.append((w, tag))
 
 # stem and lower each word
@@ -47,10 +43,8 @@ print(len(all_words), "unique stemmed words:", all_words)
 X_train = []
 y_train = []
 for (pattern_sentence, tag) in xy:
-    # X: bag of words for each pattern_sentence
     bag = bag_of_words(pattern_sentence, all_words)
     X_train.append(bag)
-    # y: PyTorch CrossEntropyLoss needs only class labels, not one-hot
     label = tags.index(tag)
     y_train.append(label)
 
@@ -72,11 +66,9 @@ class ChatDataset(Dataset):
         self.x_data = X_train
         self.y_data = y_train
 
-    # support indexing such that dataset[i] can be used to get i-th sample
     def __getitem__(self, index):
         return self.x_data[index], self.y_data[index]
 
-    # we can call len(dataset) to return the size
     def __len__(self):
         return self.n_samples
 
